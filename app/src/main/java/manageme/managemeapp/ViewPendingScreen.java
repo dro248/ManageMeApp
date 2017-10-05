2d30fb574a7465a0543259768d0bfdb00825d5ee
David <david.ostler001@gmail.com>
2017-10-05 16:41:14 -0600

    initial commit

---
+++ app/src/main/java/manageme/managemeapp/ViewPendingScreen.java
@@ -0,0 +1,13 @@
+package manageme.managemeapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewPendingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pending_screen);
+    }
+}
