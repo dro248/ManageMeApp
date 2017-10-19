339d23a8fddc1a8533fe5b21239f98bb9b59ffa1
David <david.ostler001@gmail.com>
2017-10-18 23:03:59 -0600

    Firebase submission is working!

--- app/src/main/java/manageme/managemeapp/RequestForm.java
+++ app/src/main/java/manageme/managemeapp/RequestForm.java
@@ -4,19 +4,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.RuntimeExecutionException;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
-import org.json.JSONObject;
-
-import java.util.HashMap;
-import java.util.Map;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import org.w3c.dom.Text;
 
 
 public class RequestForm extends AppCompatActivity {
@@ -25,6 +24,8 @@ public class RequestForm extends AppCompatActivity {
     private Button cancelButton;
     private TextView title;
     private TextView description;
+    private RadioGroup severityGroup;
+    private String severity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,7 +35,9 @@ public class RequestForm extends AppCompatActivity {
         // Bind GUI to objects
         submitFormButton = (Button) findViewById(R.id.submitFormButton);
         cancelButton = (Button) findViewById(R.id.cancelButton);
-
+        title = (TextView) findViewById(R.id.titleText);
+        description = (TextView) findViewById(R.id.descriptionText);
+        severityGroup = (RadioGroup) findViewById(R.id.severityGroup);
 
         // Create Event Listeners for GUI
         submitFormButton.setOnClickListener(new View.OnClickListener() {
@@ -42,28 +45,28 @@ public class RequestForm extends AppCompatActivity {
             public void onClick(View view) {
                 // Create Database Connection
                 FirebaseDatabase database = FirebaseDatabase.getInstance();
-                DatabaseReference myRef = database.getReference("request");
+                DatabaseReference myRef = database.getReference("pending_request");
                 Request myRequest = new Request("","","","");
                 Gson gson = new Gson();
 
                 // Attempt to create a Request object
                 try{
-//                    myRequest = createRequestObject();
-                    myRequest = new Request("myTitle", "descriptors...n stuff", "unviewed", "HI");
-
+                    myRequest = createRequestObject();
+                    System.out.println(gson.toJson(myRequest).toString());
                 }
                 catch(Exception e){
-                    Toast.makeText(getApplicationContext(), "Title, Description, and Severity required.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Could not submit: Title, Description, and Severity options required!", Toast.LENGTH_SHORT).show();
+                    return;
                 }
 
                 // Attempt to Submit to firebase
                 try{
                     myRef.setValue(gson.toJson(myRequest));
-                    Toast.makeText(getApplicationContext(), "Request Submitted!", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Success! Request Submitted.", Toast.LENGTH_SHORT).show();
                     finish();
                 }
                 catch(Exception e){
-                    Toast.makeText(getApplicationContext(), "Transmission Failure: Could not submit request.", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Transmission Failure: Could not submit request!", Toast.LENGTH_SHORT).show();
                 }
             }
         });
@@ -74,7 +77,28 @@ public class RequestForm extends AppCompatActivity {
 
     }
 
-//    private Request createRequestObject() throws RuntimeException{
-//
-//    }
+    private Request createRequestObject() throws RuntimeException{
+        int index = severityGroup.indexOfChild(findViewById(severityGroup.getCheckedRadioButtonId()));
+        System.out.println("Index: " + index);
+
+        switch(index){
+            case 0:
+                severity = "low";
+                break;
+            case 2:
+                severity = "med";
+                break;
+            case 4:
+                severity = "high";
+                break;
+            default:
+                throw new RuntimeException();
+        }
+
+        if(title.getText().equals("") || description.getText().equals(""))
+            throw new RuntimeException();
+
+        // TODO: Change STATUS
+        return new Request(title.getText().toString(), description.getText().toString(), "unviewed", severity);
+    }
 }
