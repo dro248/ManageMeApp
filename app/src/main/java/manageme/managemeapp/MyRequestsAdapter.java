ff27e7221742ce31ee08df61b0f4e0955f2c51c0
David <david.ostler001@gmail.com>
2017-10-19 09:03:43 -0600

    Adds Photos, dummy titles, and dummy view detail to cards.

--- app/src/main/java/manageme/managemeapp/MyRequestsAdapter.java
+++ app/src/main/java/manageme/managemeapp/MyRequestsAdapter.java
@@ -4,39 +4,51 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import static manageme.managemeapp.R.drawable.ic_hammer;
+import static manageme.managemeapp.R.drawable.ic_picture;
 
 
 public class MyRequestsAdapter extends RecyclerView.Adapter {
 
+    public String[] dummyTitles = new String[]{
+            "Leaky Sink",
+            "Mold Problem!",
+            "Hole in wall",
+            "I lost my key...",
+            "No internet. Please fix!",
+            "Our Neighbors are too loud..."
+    };
 
-
-
-
-
+    public String[] dummyDetails = new String[]{
+            "Unviewed",
+            "Scheduled: Nov 18",
+            "Unviewed:",
+            "Viewed: Sept 20",
+            "Viewed: Oct 18",
+            "Viewed: Oct 18"
+    };
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
         public int currentItem;
         public ImageView itemImage;
         public TextView itemTitle;
         public TextView itemDetail;
+        public Button fulfillButton;
+        public Button discardButton;
 
         public MyViewHolder(View itemLayoutView) {
             super(itemLayoutView);
             itemImage = (ImageView)itemView.findViewById(R.id.item_image);
             itemTitle = (TextView)itemView.findViewById(R.id.item_title);
             itemDetail = (TextView)itemView.findViewById(R.id.item_detail);
+            fulfillButton = (Button) itemView.findViewById(R.id.fulfillButton);
+            discardButton = (Button) itemView.findViewById(R.id.discardButton);
         }
-
     }
-
-
-
-
-
 
     public MyRequestsAdapter(Request[] requestedData) {
         super();
@@ -53,11 +65,34 @@ public class MyRequestsAdapter extends RecyclerView.Adapter {
     }
 
     @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
         //TODO: Add real values from firebase!
-        ((MyViewHolder) holder).itemTitle.setText("a title a title");
-        ((MyViewHolder) holder).itemDetail.setText("details details");
-        ((MyViewHolder) holder).itemImage.setImageResource(ic_hammer);
+        ((MyViewHolder) holder).itemTitle.setText(dummyTitles[position]);
+        ((MyViewHolder) holder).itemDetail.setText(dummyDetails[position]);
+        ((MyViewHolder) holder).itemImage.setImageResource(ic_picture);
+        ((MyViewHolder) holder).fulfillButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("FULFILL from card " + position);
+
+                // TODO: Send FULFILL signal to database
+
+                // TODO: On Success: Remove card from list
+
+
+            }
+        });
+        ((MyViewHolder) holder).discardButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                System.out.println("DISCARD from card " + position);
+
+                // TODO: Send DISCARD signal to database
+
+                // TODO: On Success: Remove card from list
+
+            }
+        });
     }
 
     @Override
